31d879ae15b4746d927d144fcd0c3b6459b1e00f
VictorBK <victorbrian4@gmail.com>
2023-04-16 19:23:57 +0300

    Master changes pushed

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// navbar toggle
+const navbarShowBtn = document.querySelector('.navbar-show-btn');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+const navbarHideBtn = document.querySelector('.navbar-hide-btn');
+
+navbarShowBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.add('navbar-show');
+});
+navbarHideBtn.addEventListener('click', function(){
+    navbarCollapseDiv.classList.remove('navbar-show');
+});
